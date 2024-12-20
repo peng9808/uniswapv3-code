577a269dbb3db231e01225fe15ae8b12396fc414
peng9808 <peng.lai@daocloud.io>
2024-12-20 16:14:46 +0800

    Add Management Contract

--- src/interfaces/IUniswapV3MintCallback.sol
+++ src/interfaces/IUniswapV3MintCallback.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.14;
 
-interface IuniswapV3MintCallback {
-    function uniswapV3MintCallback(uint256 amount0, uint256 amount1) external;
+interface IUniswapV3MintCallback {
+    function uniswapV3MintCallback(
+        uint256 amount0,
+        uint256 amount1,
+        bytes calldata data
+    ) external;
 }
